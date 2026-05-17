633312f5437f4ed5108533f5c895c7ca38c0fc5a
Branden Moya <bmoya217@gmail.com>
2026-05-16 21:07:57 -0700

    theme hydration protection

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -2,16 +2,33 @@
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+type IconProps = {
+  mounted: boolean;
+  theme?: string;
+};
+
+const Icon = ({ mounted, theme }: IconProps) => {
+  if (!mounted) return <div className="w-[18px] h-[18px]" />;
+  if (theme === "dark") return <Moon size={18} />;
+  return <Sun size={18} />;
+};
 
 export const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
     <button
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className="border p-1 rounded-full"
     >
-      {theme === "dark" ? <Moon size={18} /> : <Sun size={18} />}
+      <Icon mounted={mounted} theme={theme} />
     </button>
   );
 };
