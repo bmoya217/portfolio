313afd76c530da4d6423c7ae8e8125dacc0df1f2
Branden Moya <bmoya217@gmail.com>
2026-05-16 17:45:01 -0700

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,8 @@ export default function RootLayout({
           {children}
           <Footer />
         </ThemeProvider>
+
+        <Analytics />
       </body>
     </html>
   );
